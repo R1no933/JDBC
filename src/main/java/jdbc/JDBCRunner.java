63318848c069a08f3a6d41df09bcbe9959e92d38
R1no933 <dbaskakov93@yandex.ru>
2023-09-03 21:50:12 +0300

    add second sql req for DLM operation(work with executeUpdate)

--- src/main/java/jdbc/JDBCRunner.java
+++ src/main/java/jdbc/JDBCRunner.java
@@ -12,18 +12,29 @@ public class JDBCRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
 
-        String sqlRequest = """
+        String sqlDDL = """
                 CREATE TABLE IF NOT EXISTS info (
                     id SERIAL PRIMARY KEY ,
                     data TEXT NOT NULL 
                 )
                 """;
 
+        String sqlDML = """
+                INSERT INTO info (data)
+                VALUES
+                ('TEST_1'),
+                ('TEST_2'),
+                ('TEST_3'),
+                ('TEST_4')
+                """;
+
         try (Connection connection = ConnectionManager.openConncetion();
              Statement statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
-            boolean executeResult = statement.execute(sqlRequest);
-            System.out.println(executeResult);
+            boolean executeResultDDL = statement.execute(sqlDDL);
+            System.out.println(executeResultDDL);
+            int executeResultDML = statement.executeUpdate(sqlDML);
+            System.out.println(executeResultDML);
         }
     }
 }
