57637f9fc97e4f0e02df1540cfcb6f9f0a054cda
R1no933 <dbaskakov93@yandex.ru>
2023-09-03 21:19:23 +0300

    add statemnt for DDL operation (create table)

--- src/main/java/jdbc/JDBCRunner.java
+++ src/main/java/jdbc/JDBCRunner.java
@@ -6,13 +6,24 @@ import org.postgresql.Driver;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
 
-        try (Connection connection = ConnectionManager.openConncetion()) {
+        String sqlRequest = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL 
+                )
+                """;
+
+        try (Connection connection = ConnectionManager.openConncetion();
+             Statement statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
+            boolean executeResult = statement.execute(sqlRequest);
+            System.out.println(executeResult);
         }
     }
 }
